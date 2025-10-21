fe54f5d91d4669992c61f0c6cc613eb2428572bd
JJUANRC <jojuan2412@gmail.com>
2025-10-21 10:29:06 -0600

    actividad

--- SPOTIFY-CLON-APP-develop/SPOTIFY-CLON-APP-develop/src/app/app.ts
+++ SPOTIFY-CLON-APP-develop/SPOTIFY-CLON-APP-develop/src/app/app.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SpotifyLoginService } from './services/spotify-api/spotify-login-service';
-import { SpotifyAlbumService } from './services/spotify-api/spotify-album-service';
 import { CookiesStorageService } from './services/general/cookies-storage-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  standalone: false,
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App implements OnInit{
+export class App implements OnInit {
 
   constructor(
     private _spotifyLogin: SpotifyLoginService,
-    private _cookieStorage: CookiesStorageService
-  ){}
+    private _cookieStorage: CookiesStorageService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
-    if(!this._cookieStorage.exists('access_token') || !this._cookieStorage.isCookieValid('access_token'))
+    if (!this._cookieStorage.exists('access_token') || !this._cookieStorage.isCookieValid('access_token')) {
       this._spotifyLogin.getAccessToken().subscribe();
+    }
   }
 
+  isSearchRoute(): boolean {
+    return this.router.url.includes('/search');
+  }
 }
